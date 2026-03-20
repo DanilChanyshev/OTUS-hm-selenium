be335c230d1986bef7b8aac1b4a6d93578d1441c
CodeWeiName <128158298+CodeWeiName@users.noreply.github.com>
2026-03-21 00:02:50 +0300

    Ansible: return local selenium

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,16 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.junit5.ScreenShooterExtension;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(ScreenShooterExtension.class)
 public class BaseTest {
 
   @BeforeEach
   void setUp() {
+    Configuration.reportsFolder = "target/allure-results";
     SelenideLogger.addListener("allure", new AllureSelenide()
             .screenshots(true)
             .savePageSource(false));
